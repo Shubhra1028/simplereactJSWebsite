a1da4842590b8f017e092a9a403821659d727c1d
Shubhra1028 <shubhra1028@gmail.com>
2018-12-17 01:31:25 +0530

    dashview

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './style/App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './components/Home/home'
 
--- src/components/Home/dashview.js
+++ src/components/Home/dashview.js
@@ -2,10 +2,34 @@ import React, {Component} from 'react'
 
 class DashView extends Component{
 
+    state={
+        otherLinks: [
+            'Websites', 'Blogs', 'Features', 'Pricing', 'News', 'Support'
+        ],
+        authLinks : [
+            'Log In', 'Get Started'
+        ]
+    }
+
+    returnList = (list)=>{
+        return list.map((item)=>{
+            return <li> {item} </li>
+        })
+    }
+
     render(){
         return(
-            <div>
-                <h1>Heyya</h1>
+            <div className="dashboard">
+                <div className="topbar">
+                    <h2>Dummy Me!</h2>
+                    <ul className="navList ">
+                        {this.returnList(this.state.otherLinks)}
+                    </ul>
+                    <div className="spacer" />
+                    <ul className="navList ">
+                        {this.returnList(this.state.authLinks)}
+                    </ul>
+                </div>
             </div>
         )
     }
