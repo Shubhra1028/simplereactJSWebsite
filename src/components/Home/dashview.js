3932ada13a7ff2208a941d0983847c0ab1af3d4a
Shubhra1028 <shubhra1028@gmail.com>
2018-12-17 00:31:05 +0530

    init

---
+++ src/components/Home/dashview.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react' 
+
+class DashView extends Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Heyya</h1>
+            </div>
+        )
+    }
+}
+
+export default DashView;
---
+++ src/components/Home/home.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+import DashView from './dashview'
+
+const Home = ()=>{
+    return(
+        <div>
+            <DashView />
+        </div>
+    )
+} 
+
+export default Home
